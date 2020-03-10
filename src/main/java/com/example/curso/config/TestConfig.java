f3c923ea55e67f597be81987692a1a6df5b1fe9c
Estêvão Cardoso Silva <estevaof10@gmail.com>
2020-03-10 14:57:04 -0300

    Payment one-to-one association

--- src/main/java/com/example/curso/config/TestConfig.java
+++ src/main/java/com/example/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.example.curso.entities.Category;
 import com.example.curso.entities.Order;
 import com.example.curso.entities.OrderItem;
+import com.example.curso.entities.Payment;
 import com.example.curso.entities.Product;
 import com.example.curso.entities.User;
 import com.example.curso.entities.enums.OrderStatus;
@@ -84,6 +85,10 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRpository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"),o1);
+		o1.setPaymnt(pay1);
+		orderRepository.save(o1);
 		
 	}
 	
--- src/main/java/com/example/curso/entities/Order.java
+++ src/main/java/com/example/curso/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.example.curso.entities.enums.OrderStatus;
@@ -38,6 +40,8 @@ public class Order implements Serializable {
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
 	
+	@OneToOne(mappedBy = "order",cascade = CascadeType.ALL)
+	private Payment paymnt;
 	
 	public Order() {
 
@@ -84,6 +88,15 @@ public class Order implements Serializable {
 		this.client = client;
 	}
 	
+	
+	public Payment getPaymnt() {
+		return paymnt;
+	}
+
+	public void setPaymnt(Payment paymnt) {
+		this.paymnt = paymnt;
+	}
+
 	public Set<OrderItem> getItems(){
 		return items;
 	}
